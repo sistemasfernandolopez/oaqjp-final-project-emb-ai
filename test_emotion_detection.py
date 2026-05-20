fb1a10e2debc2dec626c23fe8df9d727ad8c33f6
Fernando Lopez <sistemasfernandolopez@gmail.com>
2026-05-20 15:55:19 -0400

    Pruebas Unitarias test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,29 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_emotion_detector(self):
+
+        # Caso de prueba para alegría
+        result_1 = emotion_detector('I am glad this happened')
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
+
+        # Caso de prueba para ira
+        result_2 = emotion_detector('I am really mad about this')
+        self.assertEqual(result_2['dominant_emotion'], 'anger')
+
+        # Caso de prueba para desagrado
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(result_3['dominant_emotion'], 'disgust')
+
+        # Caso de prueba para tristeza
+        result_4 = emotion_detector('I am so sad about this')
+        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+
+        # Caso de prueba para miedo
+        result_5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(result_5['dominant_emotion'], 'fear')
+
+
+unittest.main()
